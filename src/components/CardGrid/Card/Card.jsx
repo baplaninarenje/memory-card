986a2d745e47d276a8db10bd205712a84ee0cd9b
baplaninarenje <157408243+baplaninarenje@users.noreply.github.com>
2024-12-20 20:25:20 +0100

    Update Card structure

--- src/components/CardGrid/Card/Card.jsx
+++ src/components/CardGrid/Card/Card.jsx
@@ -1,5 +1,18 @@
 function Card() {
-  return <></>;
+  return (
+    <button className="card" aria-label="Memory card showing TBA">
+      <figure>
+        <img
+          loading="lazy"
+          width={475}
+          height={475}
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/132.png"
+          alt="TBA"
+        />
+        <figcaption>TBA</figcaption>
+      </figure>
+    </button>
+  );
 }
 
 export default Card;
